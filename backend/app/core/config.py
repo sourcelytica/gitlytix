91cb470cb0e674963054d3f9a3021253049b4210
ZoranPandovski <zoran.pandovski@gmail.com>
2025-02-07 17:26:18 +0100

    Init core

---
+++ backend/app/core/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import  (
+    computed_field,
+    ClickHouseDsn,
+    AnyUrl
+)
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True
+    )
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "Gitlytix"
+    CLICKHOUSE_USER: str = "default"
+    CLICKHOUSE_PASSWORD: str = "default"
+    CLICKHOUSE_HOST: str = "localhost"
+    CLICKHOUSE_PORT: int = 8123
+    CLICKHOUSE_DB: str = "default"
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> ClickHouseDsn:
+        return AnyUrl.build(
+            scheme="clickhouse+connect",
+            username=self.CLICKHOUSE_USER,
+            password=self.CLICKHOUSE_PASSWORD,
+            host=self.CLICKHOUSE_HOST,
+            port=self.CLICKHOUSE_PORT,
+            path=self.CLICKHOUSE_DB,
+        )
+
+settings = Settings()
