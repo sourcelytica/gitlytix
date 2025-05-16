b6e873bd000f882f03e6d851ac5117d0685929ff
ZoranPandovski <zoran.pandovski@gmail.com>
2025-05-16 17:37:31 +0200

    Add new routes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import stats
+from app.api.routes import stats, issues, prs
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(stats.router)
-
+api_router.include_router(issues.router)
+api_router.include_router(prs.router)
